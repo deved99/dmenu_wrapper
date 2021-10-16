44bb1bc6bc9e69cd3994b07037e7083ce18f302c
Davide Forlani <forla.df@gmail.com>
2021-10-16 20:49:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# custom dmenu
+This is a (very simple) wrapper around
+[bemenu](https://github.com/Cloudef/bemenu).
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::process::Command;
+
+const FOREGROUND: &str = env!("COLOR_FG");
+const BACKGROUND: &str = env!("COLOR_BG");
+const ACCENT: &str = env!("COLOR_A");
+
+fn main() {
+    match Command::new("bemenu")
+        .args(&[
+              "--ignorecase",
+              "--list=10", "--bottom",
+              "--fn", "Noto Sans 15",
+              "--tb", ACCENT,    // Title background.
+              "--tf", BACKGROUND,    // Title foreground.
+              "--fb", ACCENT,    // Filter background.
+              "--ff", BACKGROUND,    // Filter foreground
+              "--nb", BACKGROUND,    // Normal background.
+              "--nf", FOREGROUND,    // Normal foreground.
+              "--hb", ACCENT,    // Highlighted background.
+              "--hf", BACKGROUND,    // Highlighted foreground.
+              "--sb", ACCENT,    // Selected background.
+              "--sf", BACKGROUND    // Selected foreground.
+        ])
+        .status() {
+            Err(why) => panic!("Error running dmenu's process: {}", why),
+            Ok(_) => ()
+	};
+}
